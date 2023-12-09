fb1ab5b553f3c53bf571a7bfef4f3f5ae2ad7654
tkatcho <tkatcho@outlook.com>
2023-12-09 14:10:42 -0500

    TRES GROS COMMIT
    Spawn, remove enemies dynamically, partie "AUTRE" completer, flags avec maillage et prog procedural, rendu au mouvement des enemies, object pool et bcp d'autres choses. GAME LOOP MANAGER!!!! CONTROLE QUOI LES PERSONNAGES DEVRONT FAIRE AINSI DE LES SPAWNERES, DESPAWNERS. AUSSI ON A ENNEMIESPAWNER ET AUTRES

---
+++ Assets/scripts/flag/WavingFlagComponent.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class WavingFlagComponent : MonoBehaviour
+{
+    [SerializeField] float Height;
+    [SerializeField] float Width;
+    [SerializeField] float nbVertexLarge;
+    [SerializeField] float nbVertexHaut;
+    public Material material;
+
+    public float waveFrequency = 1.0f;
+    public float waveAmplitude = 1.0f;
+    public float waveSpeed = 1.0f;
+    public float waveHeight = 1.0f;
+    private Vector3[] originalVertices;
+    private Mesh mesh;
+    private MeshFilter mFilter;
+    void Start()
+    {
+        mesh = new();
+        mesh.vertices = GenerateVertices();
+        mesh.triangles = GenerateTriangles();
+        mesh.uv = GenerateUv();
+        mesh.RecalculateNormals();
+        mFilter = GetComponent<MeshFilter>();
+
+        Renderer renderer = GetComponent<Renderer>();
+
+        if (material != null)
+        {
+            renderer.material = material;
+        }
+        mFilter.mesh = mesh;
+        originalVertices = mesh.vertices.Clone() as Vector3[];
+
+    }
+
+    private Vector2[] GenerateUv()
+    {
+        int cpt = 0;
+        Vector2[] X = new Vector2[(int)(nbVertexHaut * nbVertexLarge)];
+        float distanceX = 1.0f / (nbVertexLarge - 1);
+        float distanceY = 1.0f / (nbVertexHaut - 1);
+
+        for (float y = 0; y < nbVertexHaut; y++)
+        {
+            for (int x = 0; x < nbVertexLarge; x++)
+            {
+                X[cpt] = new Vector2(x * distanceX, 1 - y * distanceY);
+                cpt++;
+            }
+        }
+
+        return X;
+    }
+    private int[] GenerateTriangles()
+    {
+        int numTriangles = (int)((nbVertexLarge - 1) * (nbVertexHaut - 1) * 6);
+        int[] triangles = new int[numTriangles];
+        int index = 0;
+
+        for (int y = 0; y < nbVertexHaut - 1; y++)
+        {
+            for (int x = 0; x < nbVertexLarge - 1; x++)
+            {
+                int currentVertex = (int)(x + y * nbVertexLarge);
+                triangles[index++] = currentVertex;
+                triangles[index++] = (int)(currentVertex + nbVertexLarge);
+                triangles[index++] = currentVertex + 1;
+
+                triangles[index++] = currentVertex + 1;
+                triangles[index++] = (int)(currentVertex + nbVertexLarge);
+                triangles[index++] = (int)(currentVertex + nbVertexLarge + 1);
+            }
+        }
+
+        return triangles;
+    }
+    private Vector3[] GenerateVertices()
+    {
+        int cmpt = 0;
+        float distanceX = Width / nbVertexLarge;
+        float distanceY = Height / nbVertexHaut;
+        Vector3[] X = new Vector3[(int)(nbVertexHaut * nbVertexLarge)];
+
+        for (float y = nbVertexHaut; y > 0; y--)
+        {
+            for (int x = 0; x < nbVertexLarge; x++)
+            {
+                X[cmpt] = new Vector3(x * distanceX, y * distanceY);
+                cmpt++;
+            }
+        }
+        return X;
+    }
+
+
+
+    void Update()
+    {
+        Vector3[] vertices = mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 originalVertex = originalVertices[i];
+            float waveOffset = Mathf.Sin(Time.time * waveSpeed + originalVertex.x) * waveHeight;
+            vertices[i] = originalVertex + Vector3.up * waveOffset;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/scripts/game/GameLoopManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoopManager : MonoBehaviour
+{
+    public static Vector3[] NodePositions;
+
+    private static Queue<int> EnemysToSummon;
+    private static Queue<Enemy> EnemysToRemove;
+    public bool LoopShouldEnd;
+    public Transform NodeParent;
+
+    private void Start()
+    {
+        EnemysToSummon = new Queue<int>();
+        EnemysToRemove = new Queue<Enemy>();
+        EntitySummon.Init();
+
+        NodePositions = new Vector3[NodeParent.childCount];
+
+        for(int i=0; i < NodePositions.Length;i++)
+        {
+            NodePositions[i] = NodeParent.GetChild(i).position;
+        }
+
+        StartCoroutine(GameLoop());
+        InvokeRepeating("SummonTest",0f,1f);
+        InvokeRepeating("RemoveTest", 0f, 2f);
+
+    }
+    void RemoveTest()
+    {
+        if (EntitySummon.EnemiesInGame.Count > 0)
+        {
+            EntitySummon.RemoveEnemy(EntitySummon.EnemiesInGame[Random.Range(0,EntitySummon.EnemiesInGame.Count)]);
+        }
+    }
+    void SummonTest()
+    {
+        EnqueueEnemyIdSummon(1);
+    }
+    IEnumerator GameLoop()
+    {
+        while (!LoopShouldEnd)
+        {
+            //spawnEnemies
+            if(EnemysToSummon.Count > 0)
+            {
+                for(int i =0; i < EnemysToSummon.Count; i++)
+                {
+                    EntitySummon.SummonEnemy(EnemysToSummon.Dequeue());
+                }
+            }
+
+
+
+
+            //removeEnemies
+            if (EnemysToSummon.Count > 0)
+            {
+                for (int i = 0; i < EnemysToRemove.Count; i++)
+                {
+                    EntitySummon.RemoveEnemy(EnemysToRemove.Dequeue());
+                }
+            }
+            yield return null;
+        }
+    }
+    public static void EnqueueEnemeyToRemove(Enemy enemy)
+    {
+        EnemysToRemove.Enqueue(enemy);
+    }
+    public static void EnqueueEnemyIdSummon(int ID)
+    {
+        EnemysToSummon.Enqueue(ID);
+    }
+}
