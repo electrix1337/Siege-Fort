fab8afddcef1033f4d3ad3a898e58a53b89ad875
william Bégin <energie1000@outlook.com>
2023-12-18 21:41:58 -0500

    ok super

--- Assets/graphic/textures/ressources image/copyright info.cs
+++ Assets/graphic/textures/ressources image/copyright info.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class copyrightinfo : MonoBehaviour
 {
-    //voici les droit d'auteur pour les images importé
+    //voici les droit d'auteur pour les images importé comme demander par le site freepik
     //icone de ressources
     //https://www.freepik.com/free-vector/coloured-gems-collection_951513.htm#query=gemstone&position=28&from_view=search&track=sph&uuid=33aacdf3-ac2c-4c0f-93bf-a2e77ce8a4c3
     //https://www.freepik.com/free-vector/stack-steel-pipes-isolated_9396039.htm#page=3&query=metal%20ingot&position=39&from_view=search&track=ais&uuid=db6a4c85-f61d-4940-9dab-0dfd1da19358
     /*https://www.freepik.com/free-vector/stones-rocks-cartoon_13050167.htm#query=stone&position=1&from_view=search&track=sph&uuid=b3ed2920-9573-4d3c-9b77-c26ebbf1bf72
      * https://www.freepik.com/free-vector/stones-rocks-cartoon_13050167.htm#query=stone&position=1&from_view=search&track=sph&uuid=b3ed2920-9573-4d3c-9b77-c26ebbf1bf72
      * https://www.freepik.com/free-vector/firewood-flat-elements-collection-with-tree-stump-branches-cut-logs-circular-segments-planks-campfire-isolated_6804315.htm#page=3&query=wood&position=1&from_view=search&track=sph&uuid=7be98d6f-b1b9-4fea-8237-672a754cd4fe
-     * background
+     * 
+     * textures:
+     * https://www.freepik.com/free-vector/cartoon-stone-texture_976364.htm#query=stone&position=8&from_view=search&track=sph&uuid=8a8cb345-bd73-4e3a-b820-67284e0b9653
      * https://www.freepik.com/free-vector/realistic-wood-texture-detail_22072158.htm#page=3&query=wood&position=26&from_view=search&track=sph&uuid=7be98d6f-b1b9-4fea-8237-672a754cd4fe
-     * */
+     * https://www.freepik.com/free-vector/decorative-pink-mandala-white-background_32778615.htm#query=ritual&position=16&from_view=search&track=sph&uuid=9fd411e0-52db-4561-9143-503c390cbf6b
+     * https://www.freepik.com/free-vector/vector-seamless-rippled-swimming-pool-abstract-illustration-horizontally-vertically-repeatable_38449309.htm#query=water&position=10&from_view=search&track=sph&uuid=a00bf4d9-f032-4e7d-95a5-a012f20accc3
+     * 
+     * 
+     */
 
 
 }
